19c1d8c1f469c3708193290c0764ee24a1044af6
Rojal Bati <54777753+rojalbati@users.noreply.github.com>
2021-07-29 14:29:25 +0545

    add tests

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -7,7 +7,21 @@ context('Actions', () => {
 
   // https://on.cypress.io/interacting-with-elements
 
-  it('.focus() - focus on a DOM element', () => {
+  it('.focus() - focus on a DOM element 1', () => {
+    // https://on.cypress.io/focus
+    cy.get('.action-focus').focus()
+      .should('have.class', 'focus')
+      .prev().should('have.attr', 'style', 'color: orange;')
+  })
+  
+  it('.focus() - focus on a DOM element 2', () => {
+    // https://on.cypress.io/focus
+    cy.get('.action-focus').focus()
+      .should('have.class', 'focus')
+      .prev().should('have.attr', 'style', 'color: orange;')
+  })  
+
+  it('.focus() - focus on a DOM element 3', () => {
     // https://on.cypress.io/focus
     cy.get('.action-focus').focus()
       .should('have.class', 'focus')
